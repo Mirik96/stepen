765e211c1eb821356430b20b954cf71a56d97e47
mirik <123@mail.ru>
2022-01-08 23:54:10 +0500

    all

---
+++ script.js
@@ -0,0 +1,9 @@
+let number = +prompt('Введите число');
+let stepen = +prompt('Введите степень числа');
+let answer = 1;
+
+for(let i = 0; i < stepen; i++){
+    answer = answer * number
+
+}
+alert(answer)
